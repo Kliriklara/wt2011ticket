003abd00910036731fcb701eccbe2a2723659fa6
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2011-11-16 11:14:08 +0100

    Schritt 1 bis 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 TicketApp::Application.routes.draw do
+
+  get "tickets/index"
+  get "tickets/show"
+
+  resources :tickets
+  root :to => 'tickets#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +55,6 @@ TicketApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
