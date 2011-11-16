db4fc45530359e3d035bf05101473890d8c89156
Michael Raidel <raidel@induktiv.at>
2011-11-16 10:30:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketApp::Application.config.secret_token = '644562c2d0bed0650b5e431dba0f7ab9d4184d92d57d62d525209c04e1b73805890d1a349a66f38ddd0cfa0808834a0f575161ca95bd8f945305283df5416730'
