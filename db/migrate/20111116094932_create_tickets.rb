c3b7151227ee6bd41951982b87fe4e8d56f6424d
Nicole Buchegger <nbuchegger.mmt-b2010@fh-salzburg.ac.at>
2011-11-16 10:51:45 +0100

    2) Ticket anlegen

---
+++ db/migrate/20111116094932_create_tickets.rb
@@ -0,0 +1,11 @@
+class CreateTickets < ActiveRecord::Migration
+  def change
+    create_table :tickets do |t|
+      t.integer :ticket_id
+      t.string :subject
+      t.text :request
+
+      t.timestamps
+    end
+  end
+end
