a7c900a56abfc3b6fcf9723ed2bbdba707aef66d
Nicole Buchegger <nbuchegger.mmt-b2010@fh-salzburg.ac.at>
2011-11-17 19:06:07 +0100

    4) Detailansicht

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,5 +3,9 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.all
   end
+  
+  def show
+    @tickets = Ticket.find( params[:id] )
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 TicketApp::Application.routes.draw do
 
+  get "tickets/show"
+
+  get "tickets/detail"
+
 	resources :tickets
 	
 end
