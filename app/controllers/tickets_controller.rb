664653c3dac4230e46539ac5b849b4192917b1b6
martin.tiefengrabner <mtiefengrabner.mmt-m2011@fh-salzburg.ac.at>
2011-11-19 14:58:13 +0100

    übung

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,25 +3,18 @@ class TicketsController < ApplicationController
   	@tickets = Ticket.all();
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
   def show
 	@ticket = Ticket.find(params[:id])
   end
 
-  # GET /articles/new
-  # GET /articles/new.json
   def new
     @ticket = Ticket.new
   end
 
-  # GET /articles/1/edit
   def edit
     @ticket = Ticket.find(params[:id])
       end
 
-  # POST /articles
-  # POST /articles.json
   def create
     Ticket.create(:subject=>params[:ticket][:subject], :request=>params[:ticket][:request])
     
@@ -35,8 +28,6 @@ class TicketsController < ApplicationController
     redirect_to tickets_path
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
   def destroy
     Ticket.find(params[:id]).destroy
     redirect_to tickets_path
